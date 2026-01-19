1d0b8cfe7a8341435992b515ccd56d8b08bd4e16
slothkong <jorge.mlr.elec@gmail.com>
2026-01-19 07:10:37 +0000

    Correct past according to renamed test automation file

--- .github/src/orchestrator/main.py
+++ .github/src/orchestrator/main.py
@@ -36,7 +36,7 @@ HEADERS = {
 }
 
 CATALOG_FILE = f"{environ['GITHUB_WORKSPACE']}/items.yaml"
-SUMMARY_TEMPLATE_FILE = f"{environ['GITHUB_WORKSPACE']}/.github/src/summary.template.md"
+SUMMARY_TEMPLATE_FILE = f"{environ['GITHUB_WORKSPACE']}/.github/src/orchestrator/summary.template.md"
 RED_LIGHT = "🔴"
 YELLOW_LIGHT = "🟡"
 GREEN_LIGHT = "🟢"
