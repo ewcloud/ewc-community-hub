ea046afd96e0794121e549a83e88a7101dd5033a
slothkong <jorge.mlr.elec@gmail.com>
2026-02-16 17:24:01 +0000

    Extend support for dispatching workflows downstream to ewccli repository

--- .github/scripts/orchestrator/main.py
+++ .github/scripts/orchestrator/main.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
+# Orchestration for downstream concurrent test deployment workflows.
 
-# Orchestration of downstream concurrent workflows to test Items deployment, filtered by the following conditions:
-# 1. the value of the `ITEM_TECHNOLOGY_ANNOTATIONS`env var is a subset the `annotations[].technology` attribute values
-# 2. the value of the `ITEM_OTHERS_ANNOTATIONS`env var is a subset of the `annotations[].others` attribute values
+# Filtering Methods
+# Items to be tested are filtered by a combination (logical AND operation) of the following conditions:
+# 1. the value of the `ITEM_TECHNOLOGY_ANNOTATIONS`env var is equivalent to the `annotations[].technology` attribute values
+# 2. the value of the `ITEM_OTHERS_ANNOTATIONS`env var is equivalent of the `annotations[].others` attribute values
 # 3. the presence of the `values` attribute in the Item's metadata
-# 4. (optional) the `items.<item key>.name` attribute values is in the list of names in the `ITEM_NAMES`env var
+# 4. (optional) the `items.<item key>.name` attribute values is in the list of names in the `ITEM_NAMES`env var, if set
 #
-# It forwards as input any attributes/values of the `items.<item key>.values` metadata within `items.yaml` BUT
-# adding a mutation: `inputSpec` is serialized and forwarded downstream as JSON string value for a new `inputSpecJson` attribute.
-# ( see example of compatible downstream workflow at https://github.com/ewcloud/ewc-gh-action-test-deploy-ansible-playbook/tree/v2 ).
+# Deployment Methods
+# Tests are dispatched according to the metadata annotations `others`. The deployment options include Items' native (Ansible CLI,
+# Terraform CLI, etc.) OR EWCCLI:
+#
+# 1. Native: If deploying Items via their native method, the orchestrator forwards as input any attributes/values of the
+# `items.<item key>.values` metadata within `items.yaml` BUT adding a mutation: `inputSpec` attributed is serialized and
+# forwarded downstream as JSON-string with attribute name `inputSpecJson`. See example compatible downstream workflow file at
+# https://github.com/ewcloud/ewc-ansible-playbook-flavours-and-provisioning/blob/76abcbad92b9122866c91d9ed1d9a0b35dc490e3/.github/workflows/test-ecmwf.yml#L1-L95
+#
+# 2. EWCCLI: If deploying via the EWCCLI, the orchestrator only forwards the Item name and the git ref of the branch where the
+# orchestrator was triggered. Any additional attributes/values are parsed by the EWCCLI directly from the catalog in the downstream
+# workflow. See example compatible downstream workflow file at
+# https://github.com/ewcloud/ewccli/blob/3405f8bf2aa458c5efaca8d646dff0a54d7191ee/.github/workflows/test-deployment-ansible-ecmwf.yml#L1-L63
+
 
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime, timedelta, timezone
@@ -41,6 +54,10 @@ HEADERS = {
 
 CATALOG_FILE = f"{environ['GITHUB_WORKSPACE']}/items.yaml"
 SUMMARY_TEMPLATE_FILE = f"{environ['GITHUB_WORKSPACE']}/.github/scripts/orchestrator/summary.template.md"
+
+EWCCLI_ANNOTATION = "EWCCLI-compatible"
+EWCCLI_GH_API_REPO_ENDPOINT = "ewcloud/ewccli"
+
 RED_LIGHT = "🔴"
 YELLOW_LIGHT = "🟡"
 GREEN_LIGHT = "🟢"
@@ -102,38 +119,38 @@ def read_spec_items() -> dict:
 
     if ITEM_NAMES:
         filtered_item_names = set(item_name.strip() for item_name in ITEM_NAMES.split(","))
-        print(f"main - Reading Items with names: {filtered_item_names}", flush=True)
+        print(f"main thread - Reading Items with names: {filtered_item_names}", flush=True)
 
     filtered_item_technology_annotations = set(
         technology_annotation.strip() for technology_annotation in ITEM_TECHNOLOGY_ANNOTATIONS.split(",")
     )
-    print(f"main - Reading Items with technology annotations: {filtered_item_technology_annotations}", flush=True)
+    print(
+        f"main thread - Reading Items with technology annotations: {filtered_item_technology_annotations}", flush=True
+    )
 
     filtered_item_others_annotations = set(
         others_annotation.strip() for others_annotation in ITEM_OTHERS_ANNOTATIONS.split(",")
     )
-    print(f"main - Reading Items with others annotations: {filtered_item_others_annotations}", flush=True)
+    print(f"main thread - Reading Items with others annotations: {filtered_item_others_annotations}", flush=True)
 
     spec_items = {}
 
     for key, item in catalog["spec"]["items"].items():
 
         name = item.get("name")
-        if name not in filtered_item_names:
+        if ITEM_NAMES and name not in filtered_item_names:
             continue
 
         values = item.get("values", {})
         if not values:
             continue
 
-        others_annotations = set((item.get("annotations", {}).get("others", ()),))
-        print(f"others annotations: {others_annotations}")
-        if not filtered_item_others_annotations.issubset(others_annotations):
+        others_annotations = set(item.get("annotations", {}).get("others", "").split(","))
+        if not filtered_item_others_annotations == others_annotations:
             continue
 
-        technology_annotations = set((item.get("annotations", {}).get("technology", ()),))
-        print(f"technology annotations: {technology_annotations}")
-        if not filtered_item_technology_annotations.issubset(technology_annotations):
+        technology_annotations = set(item.get("annotations", {}).get("technology", "").split(","))
+        if not filtered_item_technology_annotations == technology_annotations:
             continue
 
         spec_item = {subkey: item[subkey] for subkey in ["name", "version", "values", "sources"]}
@@ -141,7 +158,7 @@ def read_spec_items() -> dict:
         spec_items.update({key: spec_item})
 
     if len(spec_items) < 1:
-        print("::warning::main - Item name and annotation filtering returned no matches!", flush=True)
+        print("::warning::main thread - Item name and annotation filtering returned no matches!", flush=True)
 
     return spec_items
 
@@ -188,24 +205,33 @@ def dispatch_and_register(thread_id: str) -> None:
     item.dispatch_time = datetime.now(timezone.utc)
     max_dispatch_time = item.dispatch_time + timedelta(seconds=dispatch_delay_seconds)
 
-    dispatch = github_api(
-        "POST",
-        f"/repos/{item.owner}/{item.repo}/actions/workflows/{GH_DOWNSTREAM_WORKFLOW_FILE}/dispatches",
-        {
-            "ref": item.git_ref,
-            "inputs": item.values,
-        },
-    )
+    if EWCCLI_ANNOTATION in ITEM_OTHERS_ANNOTATIONS:
+        dispatch = github_api(
+            "POST",
+            f"/repos/{EWCCLI_GH_API_REPO_ENDPOINT}/actions/workflows/{GH_DOWNSTREAM_WORKFLOW_FILE}/dispatches",
+            {"ref": "main", "inputs": {"itemName": item.name, "catalogRef": f"{environ['GITHUB_HEAD_REF']}"}},
+        )
+    else:
+        dispatch = github_api(
+            "POST",
+            f"/repos/{item.owner}/{item.repo}/actions/workflows/{GH_DOWNSTREAM_WORKFLOW_FILE}/dispatches",
+            {
+                "ref": item.git_ref,
+                "inputs": item.values,
+            },
+        )
 
     print(
-        f"{thread_id} - Sent dispatch request for '{item}'",
+        f"{thread_id} thread - Sent dispatch request for '{item}'",
         flush=True,
     )
 
     try:
         dispatch.raise_for_status()
     except Exception as e:
-        print(f"::warning::{thread_id} - Failed to dispatch workflow for '{item}' with exception '{e}'", flush=True)
+        print(
+            f"::warning::{thread_id} thread - Failed to dispatch workflow for '{item}' with exception '{e}'", flush=True
+        )
         move_to_done(
             item,
             "DISPATCH_FAILED",
@@ -216,13 +242,19 @@ def dispatch_and_register(thread_id: str) -> None:
     buffer_seconds = 1
     sleep(dispatch_delay_seconds + buffer_seconds)
 
-    runs = github_api(
-        "GET",
-        f"/repos/{item.owner}/{item.repo}/actions/workflows/{GH_DOWNSTREAM_WORKFLOW_FILE}/runs?event=workflow_dispatch&created={item.dispatch_time.isoformat().replace("+00:00", "Z")}..{max_dispatch_time.isoformat().replace("+00:00", "Z")}",
-    )
+    if EWCCLI_ANNOTATION in ITEM_OTHERS_ANNOTATIONS:
+        runs = github_api(
+            "GET",
+            f"/repos/{EWCCLI_GH_API_REPO_ENDPOINT}/actions/workflows/{GH_DOWNSTREAM_WORKFLOW_FILE}/runs?event=workflow_dispatch&created={item.dispatch_time.isoformat().replace("+00:00", "Z")}..{max_dispatch_time.isoformat().replace("+00:00", "Z")}",
+        )
+    else:
+        runs = github_api(
+            "GET",
+            f"/repos/{item.owner}/{item.repo}/actions/workflows/{GH_DOWNSTREAM_WORKFLOW_FILE}/runs?event=workflow_dispatch&created={item.dispatch_time.isoformat().replace("+00:00", "Z")}..{max_dispatch_time.isoformat().replace("+00:00", "Z")}",
+        )
 
     print(
-        f"{thread_id} - Sent register request for '{item}'",
+        f"{thread_id} thread - Sent register request for '{item}'",
         flush=True,
     )
 
@@ -230,7 +262,8 @@ def dispatch_and_register(thread_id: str) -> None:
         runs.raise_for_status()
     except Exception as e:
         print(
-            f"::warning::{thread_id} - Failed to register workflow run for '{item}' with exception: '{e}'", flush=True
+            f"::warning::{thread_id} thread - Failed to register workflow run for '{item}' with exception: '{e}'",
+            flush=True,
         )
         move_to_done(item, "REGISTER_FAILED", f"HTTP request failed with code {runs.status_code}")
         return
@@ -240,15 +273,15 @@ def dispatch_and_register(thread_id: str) -> None:
     runs_count = len(runs)
 
     print(
-        f"{thread_id} - Registering {runs_count} run(s): '{json_dumps(runs, indent=4)[:1000]}...'",
+        f"{thread_id} thread - Registering {runs_count} run(s): '{json_dumps(runs, indent=4)[:1000]}...'",
         flush=True,
     )
 
     match runs_count:
 
         case 0:
-            print(f"::warning::{thread_id} - Failed to register workflow for item '{item}'", flush=True)
-            move_to_done(item, "REGISTER_FAILED", f"No runs match registration criteria for '{item}'")
+            print(f"::warning::{thread_id} thread - Failed to register workflow for item '{item}'", flush=True)
+            move_to_done(item, "REGISTER_FAILED", f"No runs match registration criteria for {item.name}")
 
         case 1:
             item.run_id = runs[0]["id"]
@@ -257,10 +290,10 @@ def dispatch_and_register(thread_id: str) -> None:
 
         case _:
             print(
-                f"::warning::{thread_id} - Multiple runs match registration criteria for '{item}'",
+                f"::warning::{thread_id} thread - Multiple runs match registration criteria for '{item}'",
                 flush=True,
             )
-            move_to_done(item, "REGISTER_FAILED", f"Multiple possible runs for '{item}'")
+            move_to_done(item, "REGISTER_FAILED", f"Multiple possible runs for {item.name}")
 
 
 def check_status(thread_id: str) -> None:
@@ -280,10 +313,13 @@ def check_status(thread_id: str) -> None:
         move_to_done(item, "TIMED_OUT", "Per-run deadline reached")
         return
 
-    run = github_api("GET", f"/repos/{item.owner}/{item.repo}/actions/runs/{item.run_id}")
+    if EWCCLI_ANNOTATION in ITEM_OTHERS_ANNOTATIONS:
+        run = github_api("GET", f"/repos/{EWCCLI_GH_API_REPO_ENDPOINT}/actions/runs/{item.run_id}")
+    else:
+        run = github_api("GET", f"/repos/{item.owner}/{item.repo}/actions/runs/{item.run_id}")
 
     print(
-        f"{thread_id} - Sent check request for '{item}'",
+        f"{thread_id} thread - Sent check request for '{item}'",
         flush=True,
     )
 
@@ -291,7 +327,7 @@ def check_status(thread_id: str) -> None:
         run.raise_for_status()
     except Exception as e:
         print(
-            f"::warning::{thread_id} - Failed to check status for '{item}' (run ID: '{item.run_id}') with exception: '{e}'",
+            f"::warning::{thread_id} thread - Failed to check status for {item.name} (run ID: '{item.run_id}') with exception: '{e}'",
             flush=True,
         )
         in_progress.put(item)
@@ -300,7 +336,7 @@ def check_status(thread_id: str) -> None:
     run = run.json()
 
     print(
-        f"{thread_id} - Checking run: '{json_dumps(run, indent=4)[:1000]}...'",
+        f"{thread_id} thread - Checking run: '{json_dumps(run, indent=4)[:1000]}...'",
         flush=True,
     )
 
@@ -315,7 +351,7 @@ def check_status(thread_id: str) -> None:
 
 
 def reduce_summarize(spec_items: dict) -> None:
-    print(f"main - {pending.qsize()} pending, {in_progress.qsize()} in progress, {done.qsize()} done")
+    print(f"main thread - {pending.qsize()} pending, {in_progress.qsize()} in progress, {done.qsize()} done")
 
     for q in (pending, in_progress):
         while not q.empty():
@@ -327,9 +363,15 @@ def reduce_summarize(spec_items: dict) -> None:
         item = done.get_nowait()
         items.append(item)
 
-    site = (
-        GH_DOWNSTREAM_WORKFLOW_FILE.split(".")[0].replace("test-", "").upper()
-    )  # turns a string like `test-ecmwf.yml` into `ECMWF`
+    if "ECMWF" in GH_DOWNSTREAM_WORKFLOW_FILE.upper():
+        site = "ECMWF"
+    elif "EUMETSAT" in GH_DOWNSTREAM_WORKFLOW_FILE.upper():
+        site = "EUMETSAT"
+    else:
+        site = "UNKNOWN"
+        print(
+            f"::warning::main thread - Unable to parse site from GH_DOWNSTREAM_WORKFLOW_FILE. By convention, the workflow filename should include any of: ['ecmwf', 'eumetsat']. Got: '{GH_DOWNSTREAM_WORKFLOW_FILE}'"
+        )
 
     status_rows = []
     for item in items:
@@ -342,11 +384,12 @@ def reduce_summarize(spec_items: dict) -> None:
             "TIMED_OUT": f"`timing out` {YELLOW_LIGHT}",
         }.get(item.state, f"`unknown` {GRAY_LIGHT}")
 
-        run_link = (
-            f"[{item.run_id}](https://github.com/{item.owner}/{item.repo}/actions/runs/{item.run_id})"
-            if item.run_id
-            else "—"
-        )
+        if not item.run_id:
+            run_link = "—"
+        elif EWCCLI_ANNOTATION in ITEM_OTHERS_ANNOTATIONS:
+            run_link = f"[{item.run_id}](https://github.com/{EWCCLI_GH_API_REPO_ENDPOINT}/actions/runs/{item.run_id})"
+        else:
+            run_link = f"[{item.run_id}](https://github.com/{item.owner}/{item.repo}/actions/runs/{item.run_id})"
 
         row = (
             f"| {state_viz} **{item.name}** "
@@ -361,18 +404,18 @@ def reduce_summarize(spec_items: dict) -> None:
 
     template_path = Path(SUMMARY_TEMPLATE_FILE)
     if not template_path.is_file():
-        print(f"::error::main - Cannot find summary template at {template_path}", flush=True)
+        print(f"::error::main thread - Cannot find summary template at {template_path}", flush=True)
         return
 
     try:
         template_content = template_path.read_text(encoding="utf-8")
     except Exception as e:
-        print(f"::error::main - Failed to read template: {e}", flush=True)
+        print(f"::error::main thread - Failed to read template: {e}", flush=True)
         return
 
     summary_contents = template_content.format(
         github_run_id=environ["GITHUB_RUN_ID"],
-        github_ref_name=environ["GITHUB_REF_NAME"],
+        github_ref_name=environ["GITHUB_HEAD_REF"],
         site=site,
         status_row=status_table_content,
         execution_plan=json_dumps(spec_items, indent=2),
@@ -382,7 +425,7 @@ def reduce_summarize(spec_items: dict) -> None:
     try:
         summary_path.write_text(summary_contents, encoding="utf-8")
     except Exception as e:
-        print(f"::error::main - Failed to write summary: {e}", flush=True)
+        print(f"::error::main thread - Failed to write summary: {e}", flush=True)
 
 
 # --- Worker Thread ---
@@ -390,13 +433,13 @@ def reduce_summarize(spec_items: dict) -> None:
 
 def dispatcher(thread_id: str) -> None:
 
-    print(f"{thread_id} - Starting thread", flush=True)
+    print(f"{thread_id} thread - Starting thread", flush=True)
 
     while not stop.is_set():
 
         if in_progress.full():
             print(
-                f"{thread_id} - Max concurrency reached. Will wait before dispatching new workflows...",
+                f"{thread_id} thread - Max concurrency reached. Will wait before dispatching new workflows...",
                 flush=True,
             )
             sleep(10)
@@ -405,14 +448,16 @@ def dispatcher(thread_id: str) -> None:
         try:
             dispatch_and_register(thread_id)
         except Exception as e:
-            print(f"::error::{thread_id} - Failed to dispatch workflow due to an unexpected error: {e}", flush=True)
+            print(
+                f"::error::{thread_id} thread - Failed to dispatch workflow due to an unexpected error: {e}", flush=True
+            )
             raise e
 
-    print(f"{thread_id} - Caught stop event. Exiting... ", flush=True)
+    print(f"{thread_id} thread - Caught stop event. Exiting... ", flush=True)
 
 
 def checker(thread_id: str) -> None:
-    print(f"{thread_id} - Starting thread", flush=True)
+    print(f"{thread_id} thread - Starting thread", flush=True)
 
     while not stop.is_set():
 
@@ -421,12 +466,12 @@ def checker(thread_id: str) -> None:
             sleep(POLLING_INTERVAL_SECONDS)
         except Exception as e:
             print(
-                f"::error::{thread_id} - Failed to check for workflow status due to an unexpected error: {e}",
+                f"::error::{thread_id} thread - Failed to check for workflow status due to an unexpected error: {e}",
                 flush=True,
             )
             raise e
 
-    print(f"{thread_id} - Caught stop event. Exiting... ", flush=True)
+    print(f"{thread_id} thread - Caught stop event. Exiting... ", flush=True)
 
 
 # --- Main Thread ---
@@ -451,23 +496,22 @@ def main() -> None:
     )  # include 1 additional worker to take owner ship of workflow dispatching and run id retrieving
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
 
-        executor.submit(dispatcher, thread_id="dispatcher_n0")
+        executor.submit(dispatcher, thread_id="dispatcher-0")
 
         for i in range(max_workers - 1):
-            executor.submit(checker, thread_id=f"checker_n{i}")
+            executor.submit(checker, thread_id=f"checker-{i}")
 
         sleep(30)
-
         while not pending.empty() or not in_progress.empty():
 
-            print(f"main - {pending.qsize()} pending, {in_progress.qsize()} in progress, {done.qsize()} done")
+            print(f"main thread - {pending.qsize()} pending, {in_progress.qsize()} in progress, {done.qsize()} done")
             sleep(5)
             if datetime.now(timezone.utc) >= total_deadline:
-                print("main - Total timeout reached")
+                print("main thread - Total timeout reached")
                 break
 
         stop.set()
-        print("main - Raised stop event...")
+        print("main thread - Raised stop event...")
         sleep(30)
 
     reduce_summarize(spec_items)
